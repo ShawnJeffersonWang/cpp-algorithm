8a079661e159b62500c212f79e4651eff5313c48
shawn <xw2002121@gmail.com>
2023-04-08 14:03:45 +0800

    3

--- LeetCode242.cpp
+++ LeetCode242.cpp
@@ -4,16 +4,16 @@ class Solution {
  public:
   bool isAnagram(string s, string t) {
     int record[26] = {0};
-    for (int i = 0; i < s.size();i++){
+    for (int i = 0; i < s.size(); i++) {
       record[s[i] - 'a']++;
     }
-    for (int i = 0; i < t.size();i++){
+    for (int i = 0; i < t.size(); i++) {
       record[t[i] - 'a']--;
     }
-      for (int i = 0; i < 26; i++) {
-        if(record[i]!=0){
-          return false;
-        }
+    for (int i = 0; i < 26; i++) {
+      if (record[i] != 0) {
+        return false;
+      }
     }
     return true;
   }
--- LeetCode707.cpp
+++ LeetCode707.cpp
@@ -1,35 +1,76 @@
 class MyLinkedList {
  public:
- struct LinkedNode {
-  int val;
-  LinkedNode* next;
-  LinkedNode() : val(0), next(nullptr){};
-  LinkedNode(int x) : val(x), next(nullptr){};
-  LinkedNode(int x, LinkedNode* next) : val(x), next(next){};
-};
+  struct LinkedNode {
+    int val;
+    LinkedNode* next;
+    LinkedNode() : val(0), next(nullptr){};
+    LinkedNode(int val) : val(val), next(nullptr){};
+  };
 
-
-  MyLinkedList() { _dummyHead = new LinkedNode(0);
+  MyLinkedList() {
+    _dummyHead = new LinkedNode(0);
     _size = 0;
   }
 
-  int get(int index) {}
+  int get(int index) {
+    if (index > (_size - 1) || index < 0) {
+      return -1;
+    }
+    LinkedNode* cur = _dummyHead->next;
+    while (index--) {
+      cur = cur->next;
+    }
+    return cur->val;
+  }
 
-  void addAtHead(int val) { LinkedNode* head = new LinkedNode();
-    LinkedNode* cur = _dummyHead;
-    while(cur->next)
+  void addAtHead(int val) {
+    LinkedNode* newNode = new LinkedNode(val);
+    newNode->next = _dummyHead->next;
+    _dummyHead->next = newNode;
+    _size++;
   }
 
   void addAtTail(int val) {
-
+    LinkedNode* newNode = new LinkedNode(val);
+    LinkedNode* cur = _dummyHead;
+    while (cur->next != nullptr) {
+      cur = cur->next;
+    }
+    cur->next = newNode;
+    _size++;
   }
 
-  void addAtIndex(int index, int val) {}
+  void addAtIndex(int index, int val) {
+    if(index>_size){
+      return;
+    }
+    LinkedNode* newNode = new LinkedNode(val);
+    LinkedNode* cur = _dummyHead;
+    while(index--){
+      cur = cur->next;
+    }
+    newNode->next = cur->next;
+    cur->next = newNode;
+    _size++;
+  }
 
-  void deleteAtIndex(int index) {}
-  private:
-   int _size;
-   LinkedNode* _dummyHead;
+  void deleteAtIndex(int index) {
+    if(index>_size-1||index<0){
+      return;
+    }
+    LinkedNode* cur = _dummyHead;
+    while (index--) {
+      cur = cur->next;
+    }
+    LinkedNode* tmp = cur->next;
+    cur->next = cur->next->next;
+    delete tmp;
+    _size--;
+  }
+
+ private:
+  int _size;
+  LinkedNode* _dummyHead;
 };
 
 /**
