37fff8657197319ea4a453ba58b5aaaca245aa55
shawn <xw2002121@gmail.com>
2023-04-11 23:15:39 +0800

    2

---
+++ LeetCode59.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    vector<vector<int>> generateMatrix(int n){
+        vector<vector<int>> res(n,vector<int>(n,0));
+    }
+};
