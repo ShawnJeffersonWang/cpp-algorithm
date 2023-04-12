2e7aca90203f4c47f796f43ee2f77ecfc89ecef7
shawn <xw2002121@gmail.com>
2023-04-12 09:09:23 +0800

    3

---
+++ LeetCode20.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    bool isValid(string s){
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') st.push(')');
+            else if(s[i]=='[') st.push(']');
+            else if(s[i]=='{') st.push('}');
+            else if(st.empty()||s[i]!=st.top()) return false;
+            else st.pop();
+        }
+        return st.empty();
+    }
+};
