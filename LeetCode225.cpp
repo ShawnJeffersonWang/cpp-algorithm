9016888f853301ab9bac4e43cde77c99fb3c2090
shawn <xw2002121@gmail.com>
2023-04-12 08:49:07 +0800

    2

---
+++ LeetCode225.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+//使用一个队列实现栈
+class MyStack{
+    public:
+    queue<int> que;
+    MyStack(){
+
+    }
+    void push(int x){
+        que.push(x);
+    }
+    int pop(){
+        int size=que.size();
+        size--;
+        while(size--){
+            que.push(que.front());
+            que.pop();
+        }
+        int result=que.front();
+        que.pop();
+        return result;
+    }
+    int top(){
+        return que.back();
+    }
+    bool empty(){
+        return que.empty();
+    }
+};
