c96c39c57b9763564e18efa2932693466c528793
shawn <xw2002121@gmail.com>
2023-04-18 17:31:41 +0800

    1

--- LeetCode101.cpp
+++ LeetCode101.cpp
@@ -10,8 +10,27 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right)
         : val(x), left(left), right(right) {}
 };
-
+// 对称二叉树
 class Solution {
    public:
-    bool isSymmetric(TreeNode* root) {}
+    bool compare(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right != nullptr) {
+            return false;
+        } else if (left != nullptr && right == nullptr) {
+            return false;
+        } else if (left == nullptr && right == nullptr) {
+            return true;
+        } else if (left->val != right->val) {
+            return false;
+        }
+        bool outside = compare(left->left, right->right);
+        bool inside = compare(left->right, right->left);
+        bool isSame = outside && inside;
+        return isSame;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        return compare(root->left, root->right);
+    }
 };
--- LeetCode226.cpp
+++ LeetCode226.cpp
@@ -10,7 +10,7 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right)
         : val(x), left(left), right(right) {}
 };
-
+// 递归法
 class Solution {
    public:
     TreeNode* invertTree(TreeNode* root) {
@@ -20,6 +20,28 @@ class Solution {
         swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
+        return root;
+    }
+};
+// 层序遍历
+class Solution {
+   public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> que;
+        if (root != nullptr)
+            que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                swap(node->left, node->right);
+                if (node->left)
+                    que.push(node->left);
+                if (node->right)
+                    que.push(node->right);
+            }
+        }
         return root;
     }
 };
