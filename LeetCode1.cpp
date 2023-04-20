269509f7d533dc9acdfd8a4b4dd8b6402239393d
shawn <xw2002121@gmail.com>
2023-04-20 22:15:38 +0800

    algorithm practice
    Signed-off-by: shawn <xw2002121@gmail.com>

---
+++ LeetCode1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            auto iter = map.find(target - nums[i]);
+            if (iter != map.end()) {
+                return {iter->second, i};
+            }
+            map.insert(pair<int, int>(nums[i], i));
+        }
+        return {};
+    }
+};
--- LeetCode101.cpp
+++ LeetCode101.cpp
@@ -11,6 +11,8 @@ struct TreeNode {
         : val(x), left(left), right(right) {}
 };
 // 对称二叉树
+
+// 递归法
 class Solution {
    public:
     bool compare(TreeNode* left, TreeNode* right) {
@@ -32,5 +34,34 @@ class Solution {
         if (root == nullptr)
             return true;
         return compare(root->left, root->right);
+    }
+};
+
+// 迭代法
+class Solution {
+   public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        queue<TreeNode*> que;
+        que.push(root->left);
+        que.push(root->right);
+        while (!que.empty()) {
+            TreeNode* leftNode = que.front();
+            que.pop();
+            TreeNode* rightNode = que.front();
+            que.pop();
+            if (!leftNode && !rightNode) {
+                continue;
+            }
+            if (!leftNode || !rightNode || (leftNode->val != rightNode->val)) {
+                return false;
+            }
+            que.push(leftNode->left);
+            que.push(rightNode->right);
+            que.push(leftNode->right);
+            que.push(rightNode->left);
+        }
+        return true;
     }
 };
