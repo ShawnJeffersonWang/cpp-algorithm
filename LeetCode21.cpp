ccf8dff02c7cdc54ccb6050355edc69a60e05b4a
shawn <xw2002121@gmail.com>
2023-05-15 15:15:19 +0800

    5.15

--- LeetCode21.cpp
+++ LeetCode21.cpp
@@ -1,28 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//Definition for singly-linked list.
+// Definition for singly-linked list.
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
-//递归法
+// 合并两个有序链表
+// 递归法
 class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-    }
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {}
 };
-//迭代法
+// 迭代法
 class Solution {
-public:
+   public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* preHead=new ListNode(-1);
-        ListNode* prev=preHead;
-        while(list1!=nullptr&&list2!=nullptr){
-            if(list1->val<list2->val)
+        ListNode* preHead = new ListNode(-1);
+        ListNode* prev = preHead;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                prev->next = list1;
+                list1 = list1->next;
+            } else {
+                prev->next = list2;
+                list2 = list2->next;
+            }
+            prev = prev->next;
         }
+        prev->next = list1 == nullptr ? list2 : list1;
+        return preHead->next;
     }
 };
