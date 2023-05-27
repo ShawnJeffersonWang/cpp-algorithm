cf5bf66a8017faa9ee7c8e940ed992690f48320b
shawn <xw2002121@gmail.com>
2023-05-27 22:46:11 +0800

    5.27

---
+++ LeetCode376.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 摆动序列
+class Solution {
+   public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() <= 1)
+            return nums.size();
+        int curDiff = 0;
+        int preDiff = 0;
+    }
+};
