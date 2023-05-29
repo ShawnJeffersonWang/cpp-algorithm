75c206957f4f20e401f865507cac5fac62425deb
shawn <xw2002121@gmail.com>
2023-05-29 14:29:13 +0800

    5.29

--- LeetCode376.cpp
+++ LeetCode376.cpp
@@ -8,5 +8,15 @@ class Solution {
             return nums.size();
         int curDiff = 0;
         int preDiff = 0;
+        int result = 1;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            curDiff = nums[i + 1] - nums[i];
+            if ((curDiff > 0 && preDiff <= 0) ||
+                (preDiff >= 0 && curDiff < 0)) {
+                result++;
+                preDiff = curDiff;
+            }
+        }
+        return result;
     }
 };
