6961c8ec6ea9ff74f271d39633eae1bec1fdc94d
shawn <xw2002121@gmail.com>
2023-06-12 22:35:16 +0800

    6.12

--- LeetCode42.cpp
+++ LeetCode42.cpp
@@ -1,9 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <stdexcept>
 using namespace std;
 // 接雨水
+// 双指针解法
 class Solution {
-public:
+   public:
     int trap(vector<int>& height) {
-
+        int sum = 0;
+        for (int i = 0; i < height.size(); i++) {
+            if (i == 0 || i == height.size() - 1)
+                continue;
+            int rHeight = height[i];
+            int lHeight = height[i];
+            for (int r = i + 1; r < height.size(); r++) {
+                if (height[r] > rHeight)
+                    rHeight = height[r];
+            }
+            for (int l = i - 1; l >= 0; l--) {
+                if (height[l] > lHeight)
+                    lHeight = height[l];
+            }
+            int h = min(lHeight, rHeight) - height[i];
+            if (h > 0)
+                sum += h;
+        }
+        return sum;
     }
 };
