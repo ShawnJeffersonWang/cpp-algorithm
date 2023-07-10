5f4c44f51523d72186dc130b040a7f9010ed7766
shawn <xw2002121@gmail.com>
2023-07-10 14:48:40 +0800

    7.10

--- LeetCode98.cpp
+++ LeetCode98.cpp
@@ -1,27 +1,36 @@
 #include <vector>
 using namespace std;
-//Definition for a binary tree node.
+// Definition for a binary tree node.
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
 };
-
+// 验证二叉搜索树
+// 递归法
 class Solution {
-    private:
+   private:
     vector<int> vec;
-    void traversal(TreeNode* root){
-        if(root==NULL) return;
+    void traversal(TreeNode* root) {
+        if (root == NULL)
+            return;
         traversal(root->left);
         vec.push_back(root->val);
-
+        traversal(root->right);
     }
-public:
+
+   public:
     bool isValidBST(TreeNode* root) {
         vec.clear();
-
+        traversal(root);
+        for (int i = 1; i < vec.size(); i++) {
+            if (vec[i] <= vec[i - 1])
+                return false;
+        }
+        return true;
     }
 };
